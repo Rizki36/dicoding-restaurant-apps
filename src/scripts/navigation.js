811f4bcc9322d00372f378992c3a358c59fe3b5f
Fitra R <106715995+fitra36@users.noreply.github.com>
2023-05-07 20:27:04 +0700

    feat: add footer

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,10 +1,28 @@
 const btnOpenEl = document.querySelector(".btn-open");
 const btnCloseEl = document.querySelector(".btn-close");
+const navEl = document.querySelector(".navigation");
+const navHeight = navEl.getBoundingClientRect().height;
 
 btnOpenEl.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
+  // disable scroll
+  document.body.style.overflow = "hidden";
 });
 
 btnCloseEl.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
+  // enable scroll
+  document.body.style.overflow = "auto";
+});
+
+// add background color to nav on scroll for mobile
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (window.innerWidth > 768) return;
+
+  if (scrollHeight > navHeight) {
+    navEl.classList.add("nav-scroll");
+  } else {
+    navEl.classList.remove("nav-scroll");
+  }
 });
