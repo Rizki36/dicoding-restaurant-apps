4bbb743de219e4ae1b521e84892b1cbfeb8d96a2
Fitra R <106715995+fitra36@users.noreply.github.com>
2023-05-07 21:55:14 +0700

    feat: load food data

---
+++ src/scripts/foods.js
@@ -0,0 +1,26 @@
+import data from "../DATA.json";
+
+// get template
+const foodTemplate = document
+  .querySelector("#food-template")
+  .cloneNode(true).content;
+
+// get food list
+const foodList = document.querySelector(".foods__list");
+
+const render = () => {
+  data.restaurants.forEach((food) => {
+    const foodElement = foodTemplate.cloneNode(true);
+    // fill the template with data
+    foodElement.querySelector(".foods__item-name").textContent = food.name;
+    foodElement.querySelector(".foods__item-image img").src = food.pictureId;
+    foodElement.querySelector(".foods__item-rating span").textContent =
+      food.rating;
+    foodElement.querySelector(".foods__item-description").textContent =
+      food.description;
+    foodElement.querySelector(".foods__item-city").textContent = food.city;
+    foodList.appendChild(foodElement);
+  });
+};
+
+render();
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.scss";
 import "./navigation.js";
+import "./foods.js";
