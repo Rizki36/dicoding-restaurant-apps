250f5ce07ad8b7008beb1a96b9352733aa4df1ea
Fitra R <39044004+Rizki36@users.noreply.github.com>
2023-06-03 13:45:59 +0700

    feat: install lit

---
+++ src/views/pages/main-page.ts
@@ -0,0 +1,11 @@
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("main-page")
+class MainPage extends LitElement {
+  render() {
+    return html`<div></div>`;
+  }
+}
+
+export default MainPage;
