ef53c4d2df32802511a8bfd50061df7a3ca63f4b
Fitra R <39044004+Rizki36@users.noreply.github.com>
2023-07-12 19:47:05 +0700

    feat: add e2e for linking restaurant

--- e2e/linkingRestaurant.spec.ts
+++ e2e/linkingRestaurant.spec.ts
@@ -1,7 +1,30 @@
-const assert = require("assert");
-
 Feature("Liking Restaurant");
 
-Scenario("test something", ({ I }) => {
+Before(({ I }) => {
+  I.amOnPage("/#/favorite");
+});
+
+Scenario("showing empty liked restaurant", ({ I }) => {
+  I.see(
+    "Kamu belum menambahkan restaurant ke favorite",
+    ".favorite-page__empty__title"
+  );
+});
+
+Scenario("liking one restaurant", async ({ I }) => {
+  I.see(
+    "Kamu belum menambahkan restaurant ke favorite",
+    ".favorite-page__empty__title"
+  );
+
   I.amOnPage("/");
+
+  I.seeElement("foods-section");
+
+  const firstRestaurant = locate({
+    shadow: ["foods-section", "food-card", ".foods__item-name"],
+  }).first();
+  // const firstRestaurant = locate(".foods__item-name").first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  console.log(firstRestaurantTitle);
 });
