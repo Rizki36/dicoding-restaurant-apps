8d170ef7ec08d0f34373a6bbca19af7e70e785d2
Fitra R <39044004+Rizki36@users.noreply.github.com>
2023-07-13 02:37:51 +0700

    fix: config

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -15,40 +11,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-      minSize: 20000,
-      maxSize: 70000,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      automaticNameDelimiter: "~",
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-    minimizer: [new CssMinimizerPlugin()],
-  },
+  optimization: {},
   module: {
     rules: [
       {
         test: /\.(s(a|c)ss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.ts?$/,
@@ -76,7 +44,5 @@ module.exports = {
         },
       ],
     }),
-    new BundleAnalyzerPlugin(),
-    new MiniCssExtractPlugin(),
   ],
 };
