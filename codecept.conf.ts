444255169683af0b71b48dd1e721a73d6b0db52d
Fitra R <39044004+Rizki36@users.noreply.github.com>
2023-07-12 22:01:41 +0700

    feat: install webdriver

--- codecept.conf.ts
+++ codecept.conf.ts
@@ -11,16 +11,55 @@ const config: CodeceptJS.MainConfig = {
   tests: "e2e/**/*.spec.ts",
   output: "e2e/outputs",
   helpers: {
-    Puppeteer: {
+    // Puppeteer: {
+    //   url: "http://127.0.0.1:9000",
+    //   show: true,
+    //   windowSize: "1200x900",
+    // },
+    WebDriver: {
       url: "http://127.0.0.1:9000",
-      show: true,
-      windowSize: "1200x900",
+      browser: "chrome",
+      host: "127.0.0.1",
+      port: 9000,
+      restart: false,
+      windowSize: "1920x1680",
+      desiredCapabilities: {
+        chromeOptions: {
+          args: [/*"--headless",*/ "--disable-gpu", "--no-sandbox"],
+        },
+      },
+      // path: "/",
     },
   },
   include: {
     I: "./steps_file",
   },
   name: "restaurant-apps",
+  plugins: {
+    wdio: {
+      enabled: true,
+      services: ["selenium-standalone"],
+      seleniumArgs: {
+        version: "3.141.5", // Selenium standalone server version
+        drivers: {
+          chrome: {
+            version: "75.0.3770.140", // Chromedriver version
+          },
+        },
+      },
+      seleniumInstallArgs: {
+        version: "3.141.5",
+        baseURL: "https://selenium-release.storage.googleapis.com",
+        drivers: {
+          chrome: {
+            version: "75.0.3770.140",
+            arch: process.arch,
+            baseURL: "https://chromedriver.storage.googleapis.com",
+          },
+        },
+      },
+    },
+  },
 };
 
 exports.config = config;
